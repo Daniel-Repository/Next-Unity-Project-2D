60ad612f12f677c3d05dc2a9b3fac98b3b59d7b9
Daniel_C <danielcarrollbusiness@gmail.com>
2020-08-22 18:31:52 +0100

    Hover Highlight Ground Piece
    - Want an outline tile to appear over a ground tile once hovered over.
    - Started inital script. Successfully applying outline tile, but need to take it away once hovered off the ground tile.
    - Imported necessary sprites.
    - Create new tilemap to sit on a layer above initial map.

---
+++ Next_Unity_2D/Assets/Scripts/TileMapMouse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapMouse : MonoBehaviour {
+
+    private Tilemap tilemapGround;
+    private Tilemap tilemapHighlight;
+    public TileBase highlightGroundTile;
+    public TileBase groundTile;
+    Vector3Int prevGridPos;
+
+    private void Start() {
+        tilemapGround = GameObject.Find("TileMap_GroundPieces").GetComponent<Tilemap>();
+        tilemapHighlight = GameObject.Find("TileMap_GroundPiecesHighlight").GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int gridPos = tilemapGround.WorldToCell(mousePos);
+
+        if (tilemapGround.HasTile(gridPos)) {
+            tilemapHighlight.SetTile(gridPos, highlightGroundTile);
+            prevGridPos = gridPos;
+        } 
+    }
+}
