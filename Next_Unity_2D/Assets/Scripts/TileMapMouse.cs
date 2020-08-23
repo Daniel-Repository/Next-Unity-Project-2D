5c9e2615e16f3654dc9f1451b1304b1169c70eca
Daniel_C <danielcarrollbusiness@gmail.com>
2020-08-23 17:52:18 +0100

    Removing tile highlight
    - Highlight now gets removed from tile once you stop hovering over tile.
    - Added comments to TileMapMouse.cs (reeeeally need to keep commenting stuff).
    - Removed one test highlight sprite.

--- Next_Unity_2D/Assets/Scripts/TileMapMouse.cs
+++ Next_Unity_2D/Assets/Scripts/TileMapMouse.cs
@@ -5,11 +5,17 @@ using UnityEngine.Tilemaps;
 
 public class TileMapMouse : MonoBehaviour {
 
+    //Tilemaps
     private Tilemap tilemapGround;
     private Tilemap tilemapHighlight;
+
+    //Tiles
     public TileBase highlightGroundTile;
     public TileBase groundTile;
+
+    //Other
     Vector3Int prevGridPos;
+    bool highlightPlaced;
 
     private void Start() {
         tilemapGround = GameObject.Find("TileMap_GroundPieces").GetComponent<Tilemap>();
@@ -21,8 +27,15 @@ public class TileMapMouse : MonoBehaviour {
 
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int gridPos = tilemapGround.WorldToCell(mousePos);
+        highlightPlaced = tilemapHighlight.HasTile(gridPos);
 
-        if (tilemapGround.HasTile(gridPos)) {
+        //Removing HL from previously hovered tile
+        if (prevGridPos != null && prevGridPos != gridPos) {
+            tilemapHighlight.SetTile(prevGridPos, null);
+        }
+
+        //Adding HL tile to currently hovered tile if current hover has tile and there's no HL placed.
+        if (tilemapGround.HasTile(gridPos) && highlightPlaced == false) {
             tilemapHighlight.SetTile(gridPos, highlightGroundTile);
             prevGridPos = gridPos;
         } 
