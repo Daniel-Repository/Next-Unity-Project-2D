7b8b56f0249fe0eb40e29608813a07088dd6bec9
Daniel_C <danielcarrollbusiness@gmail.com>
2020-08-30 23:18:59 +0100

    Rain v2
    - Sim speed 0.3 --> 0.5
    - RainGen.cs --> Created script to make the width of the particle system the same as the width of the camera. Adapts depending on resolution. Needed this for consistent rain emission across different resolutions.

---
+++ Next_Unity_2D/Assets/Scripts/RainGen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainGen : MonoBehaviour {
+
+    private ParticleSystem rainSystem;
+    private Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        //Getting main cam width, converting to pixels, and setting the width of the PS to the same as the cam.
+        mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        float camWidth = mainCam.orthographicSize * mainCam.aspect * 100; //Multi by 100 as 1 unit = 100 pixels
+
+        rainSystem = this.GetComponent<ParticleSystem>();
+        var rsShape = rainSystem.shape;
+        rsShape.scale = new Vector3(camWidth, 0, 0);
+
+    }
+
+}
