b18aea2766fc2636f5a9b74b96883f1afbcdc5e7
Daniel_C <danielcarrollbusiness@gmail.com>
2020-09-27 20:14:57 +0100

    Camera positioning constraints
    - CameraMove.cs - Added constraints to how far a user can position camera. Need to allow it to scale when additional tiles are added.

--- Next_Unity_2D/Assets/Scripts/CameraMove.cs
+++ Next_Unity_2D/Assets/Scripts/CameraMove.cs
@@ -6,16 +6,20 @@ public class CameraMove : MonoBehaviour
 {
     
     public float speed = 0.1f;
+    public float maxY = 4.0f;
+    public float minY = -4.0f;
+    public float maxX = 10.0f;
+    public float minX = -10.0f;
 
     Vector3 camPos = new Vector3(0, 0, -1);
 
-    
     public float minZoom = 0.0f;
     public float maxZoom = 1250.0f;
 
     float zoom;
 
     void Update() {
+
         Move();
 
         transform.position = camPos;
@@ -27,18 +31,27 @@ public class CameraMove : MonoBehaviour
         //GetComponent<Camera>().orthographicSize = zoom;
     }
 
+    //Position the camera through the WASD keys
     void Move() {
         if (Input.GetKey(KeyCode.W)) {
-            camPos.y += speed * Time.deltaTime;
+            if (camPos.y < maxY) {
+                camPos.y += speed * Time.deltaTime;
+            }
         }
         if (Input.GetKey(KeyCode.S)) {
-            camPos.y -= speed * Time.deltaTime;
+            if (camPos.y > minY) {
+                camPos.y -= speed * Time.deltaTime;
+            }
         }
         if (Input.GetKey(KeyCode.A)) {
-            camPos.x -= speed * Time.deltaTime;
+            if (camPos.x > minX) {
+                camPos.x -= speed * Time.deltaTime;
+            }
         }
         if (Input.GetKey(KeyCode.D)) {
-            camPos.x += speed * Time.deltaTime;
+            if (camPos.x < maxX) {
+                camPos.x += speed * Time.deltaTime;
+            }
         }
     }
 
